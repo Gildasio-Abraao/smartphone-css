292523c6a0f7fcc73a30dcefb9d4f1f9a0d9ef2d
Gildasio Abraao <gildasio.abraao3@gmail.com>
2020-06-23 17:14:30 -0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var dayName = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
+var monName = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Agosto", "Outubro", "Novembro", "Dezembro"];
+var now = new Date;
+
+function Timer() {
+    let today = new Date(); 
+    let h = today.getHours();
+    let m = today.getMinutes();
+    var hours = document.getElementById('hour').innerHTML = `${h}`;
+    var min = document.getElementById('minute').innerHTML = `${m}`;
+    var text = document.getElementById('day').innerText = `${dayName[now.getDay()]}, ${monName[now.getMonth()]} ${now.getDate()}`;
+    setTimeout('Timer()', 500);
+    if(hours < 10) {
+        var hours = document.getElementById('hour').innerHTML = `0${h}`;
+    }
+    else if(min < 10) {
+        var min = document.getElementById('minute').innerHTML = `0${m}`;
+    }
+}
